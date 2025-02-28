9807ab853cef087bef13a58041ad4a0657064b1c
Chen Gang <82202402+minibear2021@users.noreply.github.com>
2025-03-01 01:00:44 +0800

    修复循环调用request的bug

--- wechatpayv3/core.py
+++ wechatpayv3/core.py
@@ -131,8 +131,8 @@ class Core():
             headers.update({'Content-Type': 'application/json'})
         headers.update({'Accept': 'application/json'})
         headers.update({'User-Agent': 'wechatpay v3 api python sdk(https://github.com/minibear2021/wechatpayv3)'})
-        wechatpay_serial = self._public_key_id if self._public_key_id else hex(self._last_certificate().serial_number)[2:].upper()
         if self._public_key_id or cipher_data:
+            wechatpay_serial = self._public_key_id if self._public_key_id else hex(self._last_certificate().serial_number)[2:].upper()            
             headers.update({'Wechatpay-Serial': wechatpay_serial})
         authorization = build_authorization(
             path,
